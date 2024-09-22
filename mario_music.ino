1801eb8811be64e0f15c827874aa6e367681b837
luizrabelo <58440393+luizrabelo@users.noreply.github.com>
2024-09-21 21:29:01 -0300

    Add files via upload

---
+++ mario_music.ino
@@ -0,0 +1,93 @@
+
+#include "pitches.h"
+
+#define SPEAKER_PIN 2
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Iniciando...");
+  pinMode(SPEAKER_PIN, OUTPUT);
+
+}
+
+void loop() {
+
+    Serial.println("Executando musica Mario Bros...");
+  
+int melody[] = {
+  NOTE_E7, NOTE_E7, 0, NOTE_E7,
+  0, NOTE_C7, NOTE_E7, 0,
+  NOTE_G7, 0, 0,  0,
+  NOTE_G6, 0, 0, 0,
+
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0,
+
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0
+};
+//Mario main them tempo
+int tempo[] = {
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+};
+int size = sizeof(melody) / sizeof(int);
+    for (int thisNote = 0; thisNote < size; thisNote++) {
+
+      // to calculate the note duration, take one second
+      // divided by the note type.
+      //e.g. quarter note = 1200 / 4, eighth note = 1200/8, etc.
+      int noteDuration = 1500 / tempo[thisNote];
+
+      tone(SPEAKER_PIN, melody[thisNote], noteDuration);
+
+      // to distinguish the notes, set NOTE_A4 minimum time between them.
+      // the note's duration + 30% seems to work well:
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+
+      noTone(SPEAKER_PIN);
+    }
+    tone(SPEAKER_PIN, NOTE_A4, 1200);
+
+    delay(300);
+
+    Serial.println("Concluido!");
+
+}
